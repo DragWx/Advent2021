601d4219192a05f87ca2697384d923d2ed834420
DragWx <zazzal22@gmail.com>
2021-12-05 22:48:34 -0500

    Add day05 phase 2

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Hi, I'm [@DragWx](https://github.com/DragWx), and these are my solutions for Adv
 Build the project, then open `index.html`. Select the day and phase from the dropdowns, paste the puzzle input on the left, click the `[RUN]` button, read the results on the right.
 
 The UI will get more and more ~festive~ as I get bored. :D
+
+## Commits
+Starting with `day05`, there will be at least two commits per puzzle: One immediately following when my phase 1 output passes, and then the same for phase 2. There may be other commits in between or following, but there will at least be those two.
--- src/day05.ts
+++ src/day05.ts
@@ -29,128 +29,269 @@ namespace day05 {
             2) Collision at exactly one point
             3) Collision at all points (both lines are the same)
         */
-        // The key is a composite of X and Y: X | (Y<<16)
+        // The key is a composite of X and Y: X | (Y<<24)
         var collisionPoints = new Map<number, number>();
         // Add a collision to the given coordinates.
         var collide = function (x: number, y: number) {
-            var key = (x & 0xFFFF) | ((y & 0xFFFF) << 16);
+            // Need to do it this way because bitwise operators are limited to 32 bits.
+            var key = (x & 0xFFFFFF) + ((y & 0xFFFFFF) * 0x1000000);
             if (collisionPoints.has(key)) {
                 collisionPoints.set(key, collisionPoints.get(key) + 1);
             } else {
                 collisionPoints.set(key, 1);
             }
         }
-        lines.forEach((my, i, all) => {
-            if (i < all.length - 1) {
-                var toCheck = lines.slice(i+1);
-                toCheck.forEach(your => {
-                    var myStart: number, myEnd: number, myPosition: number;
-                    var yourStart: number, yourEnd: number, yourPosition: number;
-                    if (my.orientation == Orientation.horizontal) {
-                        myStart = my.left;
-                        myEnd = my.right;
-                        myPosition = my.top;
-                    } else {
-                        myStart = my.top;
-                        myEnd = my.bottom;
-                        myPosition = my.left;
-                    }
-                    if (your.orientation == Orientation.horizontal) {
-                        yourStart = your.left;
-                        yourEnd = your.right;
-                        yourPosition = your.top;
-                    } else {
-                        yourStart = your.top;
-                        yourEnd = your.bottom;
-                        yourPosition = your.left;
-                    }
-                    if (my.orientation == your.orientation) {
-                        // Lines are parallel, so there's either no collision,
-                        // or there's 1 or more points overlapping.
-                        if (myPosition == yourPosition) {
-                            if ((yourEnd >= myStart) && (yourStart <= myEnd)) {
-                                // An overlap is happening. Figure out who's left is
-                                // farthest to the right, and whose right is farthest
-                                // to the left.
-                                var ovrStart: number, ovrEnd: number;
-                                if (yourStart <= myStart) {
-                                    ovrStart = myStart;
-                                } else {
-                                    ovrStart = yourStart;
-                                }
-                                if (yourEnd >= myEnd) {
-                                    ovrEnd = myEnd;
-                                } else {
-                                    ovrEnd = yourEnd;
-                                }
-                                // Add a collision for all points between ovrStart and ovrEnd.
-                                for (var currPos = ovrStart; currPos <= ovrEnd; currPos++) {
-                                    if (my.orientation == Orientation.horizontal) {
-                                        collide(currPos, my.top);
-                                    } else {
-                                        collide(my.left, currPos);
-                                    }
-                                }
-                            }
-                        }
-                    } else if (my.orientation != your.orientation) {
-                        // Lines are perpendicular. There's either no collision,
-                        // or collision on exactly one point.
-                        if ((yourPosition >= myStart) && (yourPosition <= myEnd)
-                        && (yourStart <= myPosition) && (yourEnd >= myPosition)) {
-                            if (my.orientation == Orientation.horizontal) {
-                                collide(your.left, my.top);
-                            } else {
-                                collide(my.left, your.top);
-                            }
-                        }
-                    }
-                });
+        var translateCoords = function (x: number, y: number, flip: boolean, swap: boolean, skew: boolean, reverse: boolean): {x: number, y: number} {
+            var newX = x;
+            var newY = y;
+            if (reverse) {
+                if (skew) {
+                    newY += newX;
+                }
+                if (swap) {
+                    var temp = newX;
+                    newX = newY;
+                    newY = temp;
+                }
+                if (flip) {
+                    // (1/2) Once upon a time, this was newX = -x.
+                    newX = -newX;
+                }
+            } else {
+                if (flip) {
+                    // (2/2) ...because *this* was newX = -x and I copy pasted it. It took forever to debug.
+                    newX = -newX;
+                }
+                if (swap) {
+                    var temp = newX;
+                    newX = newY;
+                    newY = temp;
+                }
+                if (skew) {
+                    newY -= newX;
+                }
             }
-        });
+            return {x: newX, y: newY};
+        }
+        var translateLine = function (line: Line, flip: boolean, swap: boolean, skew: boolean, reverse: boolean): Line {
+            var coord1 = translateCoords(line.left, line.leftY, flip, swap, skew, reverse);
+            var coord2 = translateCoords(line.right, line.rightY, flip, swap, skew, reverse);
+            return new Line(coord1.x, coord1.y, coord2.x, coord2.y);
+        }
 
-        appOut.value = `== Phase ${mode} ==\nOutput: ${collisionPoints.size}`;
+        //var debugOut = "";
+        lines.forEach((lineA, i) => {
+            var toCheck = lines.slice(i+1);
+            if (toCheck.length == 0) {
+                return;
+            }
+            toCheck.forEach(lineB => {
+                // Before doing anything else, perform bounding box collision
+                // detection between both lines.
+                if ((lineB.left > lineA.right) || (lineB.right < lineA.left) || (lineB.top > lineA.bottom) || (lineB.bottom < lineA.top)) {
+                    // There's no possible way that these two lines are
+                    // overlapping, no further calculations needed.
+                    return;
+                }
+
+                // We have three translations available (performed in this order):
+                // - Flip: Horizontal flip by negating X. Affects diagonals but not H/V
+                // - Swap: Switch X and Y coordinates. Affects H/V but not diagonals.
+                // - Skew: Subtract X from Y. Turns DR into H.
+
+                // We'll use them to ensure "my" line is always horizontal, so
+                // we can perform the following optimizations:
+                // H - H   : The only parallel line comparison.
+                // H - V   : Perpendicular intersection.
+                // H - DR  : Intersection.
+                // *H - UR : Flip, becomes H - DR.
+                // *V - V  : Swap, becomes H - H.
+                // *V - DR : Swap, skew, becomes H - DR.
+                // *V - UR : Swap, flip, becomes H - DR.
+                // *DR - DR: Skew, becomes H - H.
+                // DR - UR : Handle specially, only case where "your" line is UR.
+                // *UR - UR: Flip, skew, becomes H - H.
+                
+                // 10 cases down to 4. We can use the orientation of "your" line
+                // to determine which check to perform.
+
+                const inputLines = [lineA, lineB];
+                var coordFlip = false;  // Negate X
+                var coordSwap = false;  // Swap X and Y
+                var coordSkew = false;  // Subtract X from Y
+                var my: Line;
+                var your: Line;
+
+                //debugOut += `A: ${lineA.left},${lineA.leftY} -> ${lineA.right},${lineA.rightY}: ${Orientation[lineA.orientation]}\n`;
+                //debugOut += `B: ${lineB.left},${lineB.leftY} -> ${lineB.right},${lineB.rightY}: ${Orientation[lineB.orientation]}\n\n`;
+                
+                // We're doing a bunch of queries, starting by picking the first line.
+                if (my = inputLines.find(x => x.orientation == Orientation.horizontal)) {
+                    // One of the lines is horizontal.
+                    // H - H, V, DR, UR
+                    inputLines.splice(inputLines.indexOf(my), 1);
+                    // The only invalid configuration is H - UR, so check for that
+                    if (inputLines[0].orientation == Orientation.upRight) {
+                        // H - UR
+                        coordFlip = true;
+                        // --> H - DR
+                    }
+                    // At this point, we're guaranteed to have H - H, H - V, or H - DR.
+                } else if (my = inputLines.find(x => x.orientation == Orientation.vertical)) {
+                    // Neither line is horizontal, but one of them is vertical.
+                    // V - V, DR, UR
+                    inputLines.splice(inputLines.indexOf(my), 1);
+                    coordSwap = true;
+                    // --> H - H, DR, UR
+                    // At this point, the only invalid configuration is H - UR, so check for that
+                    if (inputLines[0].orientation == Orientation.upRight) {
+                        // H - UR
+                        coordFlip = true;
+                        // --> H - DR
+                    }
+                    // At this point, we're guaranteed to have either H - H, or H - DR.
+                } else if (my = inputLines.find(x => x.orientation == Orientation.downRight)) {
+                    // Both lines are diagonal, one is down-right.
+                    // DR - DR, UR
+                    inputLines.splice(inputLines.indexOf(my), 1);
+                    if (inputLines[0].orientation == Orientation.downRight) {
+                        // DR - DR
+                        coordSkew = true;
+                        // --> H - H
+                    }
+                    // We have either H - H or DR - UR.
+                } else if (my = inputLines.find(x => x.orientation == Orientation.upRight)) {
+                    // If we get here, both lines must be up-right.
+                    // UR - UR
+                    inputLines.splice(inputLines.indexOf(my), 1);
+                    coordFlip = true;
+                    // --> DR - DR
+                    coordSkew = true;
+                    // --> H - H
+                }
+                my = translateLine(my, coordFlip, coordSwap, coordSkew, false);
+                your = translateLine(inputLines.shift(), coordFlip, coordSwap, coordSkew, false);
+
+                //debugOut += `A': ${my.left},${my.leftY} -> ${my.right},${my.rightY}: ${Orientation[my.orientation]}\n`;
+                //debugOut += `B': ${your.left},${your.leftY} -> ${your.right},${your.rightY}: ${Orientation[your.orientation]}\n`;
+                //debugOut += "==========\n";
+
+                // My line is always horizontal. The orientation of your line
+                // determines which kind of check to perform.
+
+                // Remember, if we applied any transformations to the input lines,
+                // we need to UNDO that transformation on the output coordinates!
+                var outCoords: {x: number, y: number};
+                switch (your.orientation) {
+                    case Orientation.horizontal:
+                        // Parallel lines. This is the only case capable of generating multiple points.
+                        if (your.top == my.top) {
+                            // Figure out: rightmost left edge, leftmost right edge, collision is between these two edges.
+                            var rangeStart = (your.left > my.left) ? your.left : my.left;
+                            var rangeEnd = (your.right < my.right) ? your.right : my.right;
+                            for (var rangeX = rangeStart; rangeX <= rangeEnd; rangeX++) {
+                                outCoords = translateCoords(rangeX, my.top, coordFlip, coordSwap, coordSkew, true);
+                                collide(outCoords.x, outCoords.y);
+                            }
+                        }
+                        break;
+                    case Orientation.vertical:
+                        // Perpendicular lines. This is the only case where collision is guaranteed.
+                        // Your.left between My.left and My.right.
+                        outCoords = translateCoords(your.left, my.top, coordFlip, coordSwap, coordSkew, true);
+                        collide(outCoords.x, outCoords.y);
+                        break;
+                    case Orientation.downRight:
+                        // Intersection at 45 degrees.
+                        // Like perpendicular, except instead of Your.left, we use (Your.left + (My.top - Your.top)). My.top is guaranteed between Your.top and Your.bottom.
+                        var checkX = your.left + (my.top - your.top);
+                        if ((checkX >= my.left) && (checkX <= my.right)) {
+                            outCoords = translateCoords(checkX, my.top, coordFlip, coordSwap, coordSkew, true);
+                            collide(outCoords.x, outCoords.y);
+                        }
+                        break;
+                    case Orientation.upRight:
+                        // X-shaped collision.
+                        //                    11                1 1
+                        // We might get this: 11  and not this:  2    <-- Only one of those is overlapping!
+                        //                   1  1               1 1
+                        if ((your.left - my.left) <= (your.bottom - my.top)
+                            && (your.right - my.left) >= (your.top - my.top)) {
+                            // The lines cross.
+                            var crossPoint = ((my.right - my.left) / 2) + ((your.bottom - my.bottom) + (your.left - my.left)) / 2;
+                            if ((crossPoint % 1 == 0) && (my.left + crossPoint >= my.left) && (my.left + crossPoint <= my.right)) {
+                                outCoords = translateCoords(my.left + crossPoint, my.top + crossPoint, coordFlip, coordSwap, coordSkew, true);
+                                collide(outCoords.x, outCoords.y);
+                            }
+                        }
+                        break;
+                }
+            });
+        });
+        /* FAILING INPUT
+        494,864 -> 494,484
+        639,430 -> 99,970
+        709,56 -> 709,626
+        795,489 -> 599,685
+        */
+
+        /*debugOut += "== Collisions ==\n";
+        collisionPoints.forEach((v, k) => {
+            debugOut += `${k & 0xFFFFFF}, ${(k >> 24) & 0xFFFFFF}: ${v}\n`;
+        });*/
+        //appOut.value = debugOut;
+        appOut.value = `== Phase ${mode} ==\nOutput: ${collisionPoints.size}\n`;
     }
     enum Orientation {
         "horizontal",
         "vertical",
-        "diagonal"
+        "downRight",
+        "upRight"
     };
     class Line {
         public left: number;
         public top: number;
         public right: number;
         public bottom: number;
+        public leftY: number;
+        public rightY: number;
 
         public orientation: Orientation;
+        public isDiagonal: boolean;
 
-        constructor(left: number, top: number, right: number, bottom: number) {
-            this.left = left;
-            this.top = top;
-            this.right = right;
-            this.bottom = bottom;
-
-            if (top < bottom) {
-                this.top = top;
-                this.bottom = bottom;
+        constructor(x1: number, y1: number, x2: number, y2: number) {
+            var leftY: number, rightY: number;
+            if (x1 < x2) {
+                this.left = x1;
+                this.right = x2;
+                this.leftY = y1;
+                this.rightY = y2
             } else {
-                this.top = bottom;
-                this.bottom = top;
+                this.left = x2;
+                this.right = x1;
+                this.leftY = y2;
+                this.rightY = y1;
             }
-            if (left < right) {
-                this.left = left;
-                this.right = right;
+            if (y1 < y2) {
+                this.top = y1;
+                this.bottom = y2;
             } else {
-                this.left = right;
-                this.right = left;
+                this.top = y2;
+                this.bottom = y1;
             }
 
-            if (left == right) {
+            if (x1 == x2) {
                 this.orientation = Orientation.vertical;
-            } else if (top == bottom) {
+                this.isDiagonal = false;
+            } else if (y1 == y2) {
                 this.orientation = Orientation.horizontal;
+                this.isDiagonal = false;
+            } else if (this.leftY < this.rightY) {
+                this.orientation = Orientation.downRight;
+                this.isDiagonal = true;
             } else {
-                this.orientation = Orientation.diagonal;
+                this.orientation = Orientation.upRight;
+                this.isDiagonal = true;
             }
         }
     }
