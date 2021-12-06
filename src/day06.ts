ee4e552c2b3fb27bfa7992d75b6d1481d3e148b0
DragWx <zazzal22@gmail.com>
2021-12-06 00:47:50 -0500

    Add day 6 phase 2

--- src/day06.ts
+++ src/day06.ts
@@ -10,24 +10,49 @@ namespace day06 {
 
     function run (mode: number) {
         const fish = Array<Fish>();
+        const betterFish = Array<number>(9);
         appOut.value = `== Phase ${mode} ==\n`;
         // Input is a CSV with timer values.
         appIn.value.split(",").forEach((currValue) => {
             fish.push(new Fish(parseInt(currValue.trim())));
         });
         appOut.value += `Start: ${fish.length}\n`;
-        for (var i = 0; i < 80; i++) {
-            var newFish = 0;
-            fish.forEach(x => {
-                if (x.runDay()) {
-                    newFish++;
+        switch (mode) {
+            case 1:
+                for (var i = 0; i < 80; i++) {
+                    var newFish = 0;
+                    fish.forEach(x => {
+                        if (x.runDay()) {
+                            newFish++;
+                        }
+                    });
+                    while (newFish > 0) {
+                        fish.push(new Fish(8));
+                        newFish--;
+                    }
+                    appOut.value += `Day ${i + 1}: ${fish.length}\n`;
                 }
-            });
-            while (newFish > 0) {
-                fish.push(new Fish(8));
-                newFish--;
-            }
-            appOut.value += `Day ${i + 1}: ${fish.length}\n`;
+                break;
+            case 2:
+                // We've only got 8 timer states to worry about.
+                for (var i = 0; i < betterFish.length; i++) {
+                    betterFish[i] = 0;
+                }
+                fish.forEach(x => betterFish[x.timer]++);
+                for (var i = 0; i < 256; i++) {
+                    var temp: number;
+                    // Get 0, shift everything down one.
+                    temp = betterFish.shift();
+                    // Push this number of fish to the end (8)
+                    betterFish.push(temp);
+                    // Add these fish back to 6.
+                    betterFish[6] += temp;
+                    if ((i % 10) == 9) {
+                        appOut.value += `Day ${i + 1}: ${betterFish.reduce((p, v) => p + v, 0)}\n`;
+                    }
+                }
+                appOut.value += `End: ${betterFish.reduce((p, v) => p + v, 0)}\n`;
+                break;
         }
 
     }
