4a76ea80c5f3ac73da4cee953b6656e64382fd6f
DragWx <zazzal22@gmail.com>
2021-12-07 01:03:20 -0500

    Add day 7 phase 2

--- src/day07.ts
+++ src/day07.ts
@@ -14,17 +14,47 @@ namespace day07 {
         appIn.value.split(",").forEach((currValue) => {
             positions.push(parseInt(currValue));
         });
-        positions = positions.sort((a, b) => a - b);
-        
-        var average = positions.reduce((p, v) => p + v, 0);
-        average /= positions.length;
-        average = Math.round(average);
+        switch (mode) {
+            case 1:                
+                positions = positions.sort((a, b) => a - b);
+                
+                var average = positions.reduce((p, v) => p + v, 0);
+                average /= positions.length;
+                average = Math.round(average);
 
-        var median = positions[Math.round(positions.length / 2)];
+                var median = positions[Math.round(positions.length / 2)];
 
-        var outAvg = positions.reduce((p, v) => p + Math.abs(average - v), 0);
-        var outMed = positions.reduce((p, v) => p + Math.abs(median - v), 0);
+                var outAvg = positions.reduce((p, v) => p + Math.abs(average - v), 0);
+                var outMed = positions.reduce((p, v) => p + Math.abs(median - v), 0);
 
-        appOut.value += `Average: ${average}\nMedian: ${median}\nOutput(Avg): ${outAvg}\nOutput(Med): ${outMed}\n`;
+                appOut.value += `Average: ${average}\nMedian: ${median}\nOutput(Avg): ${outAvg}\nOutput(Med): ${outMed}\n`;
+                break;
+            case 2:
+                var doSum = function (input: number) {
+                    return (input * (input + 1))/2;
+                }
+                var calculate = function (input: Array<number>, position: number): number {
+                    return input.reduce((p, v) => p + doSum(Math.abs(position - v)), 0);
+                }
+
+                positions = positions.sort((a, b) => a - b);
+                
+                var average = positions.reduce((p, v) => p + v, 0);
+                average /= positions.length;
+                average = Math.round(average);
+
+                var median = positions[Math.round(positions.length / 2)];
+
+                appOut.value += `Average: ${average}\nMedian: ${median}\n`;
+
+                var outAvg = calculate(positions, average);
+                var outMed = calculate(positions, median);
+                for (var i = average - 5; i <= average + 5; i++) {
+                    appOut.value += `Using ${i}: ${calculate(positions, i)}\n`;
+                }
+
+                //appOut.value += `Average: ${average}\nMedian: ${median}\nOutput(Avg): ${outAvg}\nOutput(Med): ${outMed}\n`;
+                break;
+        }
     }
 };
