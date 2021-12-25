a1a4424e5fa5ae49ebced0b080685b2417236fe3
DragWx <zazzal22@gmail.com>
2021-12-25 04:31:43 -0500

    Add day 23 phase 2

--- src/day23.ts
+++ src/day23.ts
@@ -40,6 +40,8 @@ namespace day23 {
         ## 0  1  2  3  4  5  6  7  8  9  10 ##
         ####### 11 ## 12 ## 13 ## 14 #########
              ## 15 ## 16 ## 17 ## 18 ##
+             ## 19 ## 20 ## 21 ## 22 ##
+             ## 23 ## 24 ## 25 ## 26 ##
              ##-A--##-B--##-C--##-D--##     */
         // Positions 2, 4, 6, and 8 cannot be stopped on.
         // A pawn only has two moves:
@@ -54,16 +56,34 @@ namespace day23 {
         //    b. From [0..10], select the lowest value available, or the next value higher after backtracking.
         //       - but never 2, 4, 6, or 8.
         // 3. Backtrack.
+        let checkSpaces = [0, 1, 3, 5, 7, 9, 10, -1, -1, -1, -1];
         let pawns = new Map([
-            ["A", {targets: [15, 11], energyUse: 1}],
-            ["B", {targets: [16, 12], energyUse: 10}],
-            ["C", {targets: [17, 13], energyUse: 100}],
-            ["D", {targets: [18, 14], energyUse: 1000}]
+            ["A", {targetRoom: 0, energyUse: 1}],
+            ["B", {targetRoom: 1, energyUse: 10}],
+            ["C", {targetRoom: 2, energyUse: 100}],
+            ["D", {targetRoom: 3, energyUse: 1000}]
         ]);
-        let noStops = [2, 4, 6, 8];
+        let rooms = new Array(
+            {spaces: [11], top: 0, numGuests: 0, checkSpacesNum: 7},
+            {spaces: [12], top: 0, numGuests: 0, checkSpacesNum: 8},
+            {spaces: [13], top: 0, numGuests: 0, checkSpacesNum: 9},
+            {spaces: [14], top: 0, numGuests: 0, checkSpacesNum: 10}
+        )
+        // Add an additional level of depth to each room for each extra row of
+        // pawns scanned.
+        for (let i = 0; i < rooms.length; i++) {
+            for (let x = 1; x < (totalPawns / 4); x++) {
+                rooms[i].spaces.unshift(rooms[i].spaces[0] + 4);
+            }
+            rooms[i].top = rooms[i].spaces.length;
+            rooms[i].numGuests = rooms[i].spaces.length;
+            checkSpaces[rooms[i].checkSpacesNum] = rooms[i].spaces[rooms[i].top - 1];
+        }
+
         let currPath = Array<{to: number, from: number, energy: number}>();
         let reverse = false;
-        let currPawn: {targets: Array<number>, energyUse: number};
+        let currPawn: {targetRoom: number, energyUse: number};
+        let currRoom: {spaces: Array<number>, top: number, numGuests: number, checkSpacesNum: number};
 
         let totalEnergy = 0;
         let lowestEnergy = Infinity;
@@ -71,10 +91,15 @@ namespace day23 {
 
         // Scan to see if any pawns start out already solved.
         for (let currPawn of pawns) {
-            for (let currSpace of currPawn[1].targets) {
+            currRoom = rooms[currPawn[1].targetRoom];
+            for (let currSpace of currRoom.spaces) {
                 if (board[currSpace] == currPawn[0]) {
                     board[currSpace] = board[currSpace] + "!";
                     pawnsRemaining--;
+                    currRoom.numGuests--;
+                    if (currRoom.numGuests == 0) {
+                        checkSpaces[currRoom.checkSpacesNum] = -1;
+                    }
                 } else {
                     break;
                 }
@@ -85,7 +110,7 @@ namespace day23 {
             
             // We don't want to check the starting point for occupancy, but
             // every point afterwards is fair game.            
-            if (from >= 15) {
+            while (from >= 15) {
                 // Deep in room.
                 from -= 4;
                 currSteps++;
@@ -97,7 +122,7 @@ namespace day23 {
             }
 
             // These are from reverse perspective.
-            if (to >= 15) {
+            while (to >= 15) {
                 // Stepping deeper into room.
                 if (board[to]) { return null; }
                 to -= 4;
@@ -113,83 +138,111 @@ namespace day23 {
             // Scan the hallway.
             if (from < to) {
                 for (let i = from; i <= to; i++) {
-                    if (i == from) { currSteps--; continue; }
+                    if (i == from) { continue; }
                     if (board[i] && (i != from)) { return null; }
                 }
-                currSteps += to - from + 1;
+                currSteps += to - from;
             } else {
                 for (let i = to; i <= from; i++) {
-                    if (i == from) { currSteps--; continue; }
+                    if (i == from) { continue; }
                     if (board[i] && (i != from)) { return null; }
                 }
-                currSteps += from - to + 1;
+                currSteps += from - to;
             }
 
             return currSteps;
         }
 
         let checkPawnToRoom = function() {
-            for (let i = 0; i < board.length; i++) {
-                if (!board[i]) {
+            for (let x = 0; x < checkSpaces.length; x++) {
+                let i = checkSpaces[x];
+                if ((i == -1) || !board[i]) {
                     continue;
                 }
                 // Check this pawn.
                 currPawn = pawns.get(board[i]);
-                if (currPawn === undefined) {
-                    // This pawn doesn't need to move.
-                    continue;
-                }
-                for (let x = 0; x < currPawn.targets.length; x++) {
-                    if (!board[currPawn.targets[x]]) {
-                        // Space is empty.
-                        // Try to go here. If it's possible, this is the move.
-                        let steps = getSteps(currPawn.targets[x], i);
-                        if (steps !== null) {
-                            return {to: currPawn.targets[x], from: i, energy: steps * currPawn.energyUse};
+                currRoom = rooms[currPawn.targetRoom];
+                if (currRoom.numGuests == 0) {
+                    // Room only has target pawns in it, so this pawn is allowed
+                    // to move into it.
+                    let steps = getSteps(currRoom.spaces[currRoom.top], i);
+                    if (steps !== null) {
+                        let result = {to: currRoom.spaces[currRoom.top], from: i, energy: steps * currPawn.energyUse};
+                        // Push this move.
+                        currPath.push(result);
+                        // Move the pawn to the new space
+                        board[result.to] = board[result.from] + "!";
+                        board[result.from] = undefined;
+                        totalEnergy += result.energy;
+                        // Update helper variables
+                        pawnsRemaining--;
+                        currRoom.top++;
+                        checkSpaces[currRoom.checkSpacesNum] = (currRoom.numGuests == 0) ? -1 : currRoom.spaces[currRoom.top - 1];
+                        if (x >= 7) {
+                            // Pawn moved from another room, so make sure to update that room too.
+                            currRoom = rooms[x - 7];
+                            currRoom.numGuests--;
+                            currRoom.top--;
+                            checkSpaces[currRoom.checkSpacesNum] = (currRoom.numGuests == 0) ? -1 : currRoom.spaces[currRoom.top - 1];
                         }
-                    } else if (board[currPawn.targets[x]].length == 1) {
-                        // Space is occupied by a pawn which doesn't belong in
-                        // the room, therefore, this pawn isn't allowed to enter
-                        // this room right now.
-                        break;
+                        return true;
                     }
                 }
             }
             // No pawns can move to any rooms right now.
-            return null;
+            return false;
         }
 
-        let checkPawnToHallway = function(startRoomSpace: number = 11, startHallSpace: number = 0) {
-            if (startHallSpace >= 11) {
-                startHallSpace = 0;
-                startRoomSpace++;
+        let checkPawnToHallway = function(lastRoomSpace: number = 11, lastHallSpace: number = -1) {
+            lastRoomSpace = (lastRoomSpace - 11) & 3;
+            if (lastHallSpace >= 0) {
+                // Move to next checkable space.
+                lastHallSpace = checkSpaces.indexOf(lastHallSpace) + 1;
+            } else {
+                // If this is the first check of the path, start at the first hall space.
+                lastHallSpace = 0;
             }
-            for (let roomSpace = startRoomSpace; roomSpace < board.length; roomSpace++) {
-                if (!board[roomSpace]) {
+            if (lastHallSpace >= 7) {
+                // When we move past the end of the hall, go back to space 0 and check the next room.
+                lastHallSpace = 0;
+                lastRoomSpace++;
+            }
+            for (let roomNum = lastRoomSpace; roomNum < 4; roomNum++) {
+                currRoom = rooms[roomNum];
+                let roomSpace = checkSpaces[currRoom.checkSpacesNum];
+                if (roomSpace == -1) {
                     continue;
                 }
                 // Check this pawn.
                 currPawn = pawns.get(board[roomSpace]);
-                if (currPawn === undefined) {
-                    // This pawn doesn't need to move.
-                    continue;
-                }
-                for (let hallSpace = startHallSpace; hallSpace < 11; hallSpace++) {
-                    if ((noStops.indexOf(hallSpace) != -1) || (board[hallSpace])) {
-                        // Skip this space if it's occupied or a forbidden space.
+                for (let checkSpaceNum = lastHallSpace; checkSpaceNum < 7; checkSpaceNum++) {
+                    let hallSpace = checkSpaces[checkSpaceNum];
+                    if (board[hallSpace]) {
+                        // Skip this space if it's occupied.
                         continue;
                     } else {
                         // Try to go here. If it's possible, this is the move.
                         let steps = getSteps(hallSpace, roomSpace);
                         if (steps !== null) {
-                            return {to: hallSpace, from: roomSpace, energy: steps * currPawn.energyUse};
+                            let result = {to: hallSpace, from: roomSpace, energy: steps * currPawn.energyUse};
+                            // Push this move
+                            currPath.push(result);
+                            // Move the pawn to the new space
+                            board[result.to] = board[result.from];
+                            board[result.from] = undefined;
+                            totalEnergy += result.energy;
+                            // Update helper variables
+                            currRoom.numGuests--;
+                            currRoom.top--;
+                            checkSpaces[currRoom.checkSpacesNum] = (currRoom.numGuests == 0) ? -1 : currRoom.spaces[currRoom.top - 1];
+                            return true;
                         }
-                        break;
                     }
                 }
+                lastHallSpace = 0;
             }
             // No pawns can move to the hallway right now.
-            return null;
+            return false;
         }
 
         while (!reverse || currPath.length) {
@@ -214,26 +267,13 @@ namespace day23 {
 
 
                 // Check if someone can move into a room.
-                let result: {to: number, from: number, energy: number};
-                result = checkPawnToRoom();
-                if (result) {
-                    currPath.push(result);
-                    // We'll also mark that this pawn doesn't need to move further.
-                    board[result.to] = board[result.from] + "!";
-                    board[result.from] = undefined;
-                    totalEnergy += result.energy;
-                    pawnsRemaining--;
+                if (checkPawnToRoom()) {
                     continue;
                 }
                 
                 
                 // Try moving someone into the hallway.
-                result = checkPawnToHallway();
-                if (result) {
-                    currPath.push(result);
-                    board[result.to] = board[result.from];
-                    board[result.from] = undefined;
-                    totalEnergy += result.energy;
+                if (checkPawnToHallway()) {
                     continue;
                 }
                 
@@ -247,32 +287,31 @@ namespace day23 {
                 board[result.from] = board[result.to].charAt(0);
                 board[result.to] = undefined;
                 totalEnergy -= result.energy;
+                if (result.from >= 11) {
+                    // This was a "guest" pawn moving out of a room, either to
+                    // the hallway or to their home room.
+                    currRoom = rooms[(result.from - 11) & 3];
+                    currRoom.top++;
+                    currRoom.numGuests++;
+                    checkSpaces[currRoom.checkSpacesNum] = (currRoom.numGuests == 0) ? -1 : currRoom.spaces[currRoom.top - 1];
+                }
                 if (result.to >= 11) {
-                    // This was a move into a target room which we're undoing.
+                    // This was a pawn moving into their home room, either from
+                    // the hallway or from another room.
                     pawnsRemaining++;
+                    currRoom = rooms[(result.to - 11) & 3];
+                    currRoom.top--;
+                    checkSpaces[currRoom.checkSpacesNum] = (currRoom.numGuests == 0) ? -1 : currRoom.spaces[currRoom.top - 1];
                 } else {
-                    // This was a move into the hallway, which has multiple
-                    // possibilities, so try the next one.
-                    result = checkPawnToHallway(result.from, result.to + 1);
-                    if (result) {
-                        currPath.push(result);
-                        board[result.to] = board[result.from];
-                        board[result.from] = undefined;
-                        totalEnergy += result.energy;
+                    // This was a move into the hallway. We now need to examine
+                    // the next possibility of a pawn moving into a hallway.
+                    if (checkPawnToHallway(result.from, result.to)) {
                         reverse = false;
                         continue;
                     }
                 }
             }
         }
-
-        switch (mode) {
-            case 1:
-                appOut.value += `Output: ${lowestEnergy}\n`;
-                break;
-            case 2:
-                appOut.value += `This is phase 2's output.\n`;
-                break;
-        }
+        appOut.value += `Output: ${lowestEnergy}\n`;
     }
 };
